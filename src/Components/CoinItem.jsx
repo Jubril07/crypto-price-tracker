47f47a7793d1c0807e8bd64cf2e8407582672040
Mubarak Jubril <mubarakjubril07@gmail.com>
2022-11-02 13:18:05 +0100

    First commit

---
+++ src/Components/CoinItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CoinItem = ({coins}) => {
+  return (
+    <div className='coin-row'>
+        <p>{coins.market_cap_rank}</p>
+        <div className="img-symbol">
+            <img src={coins.image} alt="Image of coin" />
+            <p>{coins.symbol.toUpperCase()}</p>
+        </div>
+        <p>${coins.current_price}</p>
+        <p>{coins.price_change_percentage_24h}%</p>
+        <p className='hide-mobile'>${coins.total_volume}</p>
+        <p className='hide-mobile'>${coins.market_cap}</p>
+    </div>
+  )
+}
+
+export default CoinItem
---
+++ src/Components/Coins.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import CoinItem from './CoinItem'
+
+const Coins = ({coins}) => {
+  return (
+    <div className='container'>
+        <div className="heading">
+            <p>No</p>
+            <p className='coin-name'>Coins</p>
+            <p>Price</p>
+            <p>24h</p>
+            <p className='hide-mobile'>Volume</p>
+            <p className='hide-mobile'>Market</p>
+            
+        </div>
+        {coins.map((coins)=>{
+                return <CoinItem coins={coins} key={coins.id} />
+            })}
+    </div>
+  )
+}
+
+export default Coins
